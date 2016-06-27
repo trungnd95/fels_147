ad46f0f629cf95534b5cabc2a25525975f633240
Trung Nguyen <ndt8895@gmail.com>
2016-06-28 06:59:48 +0700

    Admin create category by ajax

---
+++ app/assets/javascripts/admin/categories.js.coffee
@@ -0,0 +1,3 @@
+$(document).ready ->
+  $('.submit_category').click ->
+    $('#addCategory').modal('hide')
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < ApplicationController
   layout "admin"
-  before_action :is_admin
+  before_action :check_admin
   def index
     @users = User.includes(:lessons, :categories).where("is_admin = ?", false)
       .paginate page: params[:page], per_page: Settings.per_page
@@ -11,8 +11,8 @@ class Admin::UsersController < ApplicationController
   end
 
   private
-    def user_params
-      params.require(:user).permit :name, :email, :password, :avatar, :activated,
-        :confirmation_code
-    end
+  def user_params
+    params.require(:user).permit :name, :email, :password, :avatar, :activated,
+      :confirmation_code
+  end
 end
